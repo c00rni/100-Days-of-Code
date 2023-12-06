393725d5b33979bb70e498631798d2b234efe1a5
corni <gmigan.a@gmail.com>
2023-12-06 16:25:17 +0100

    Used classes and add a function to check the machine resources before making a drink

--- coffee-machine/main.py
+++ coffee-machine/main.py
@@ -1,47 +1,79 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+class CoffeeMachine:
+    
+    def __init__(self):
+        self.bank = 0
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+        self.MENU = {
+            "espresso": {
+                "ingredients": {
+                    "water": 50,
+                    "coffee": 18,
+                },
+                "cost": 1.5,
+            },
+            "latte": {
+                "ingredients": {
+                    "water": 200,
+                    "milk": 150,
+                    "coffee": 24,
+                },
+                "cost": 2.5,
+            },
+            "cappuccino": {
+                "ingredients": {
+                    "water": 250,
+                    "milk": 100,
+                    "coffee": 24,
+                },
+                "cost": 3.0,
+            }
+        }
+    
+        def prompt():
+            """Prompte user by asking
+                off - Turn off the Coffee Machine by entering “off” to the prompt.
+                report - Print report.
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-bank=0
+            """
+            while True:
+                choise = input("What would you like? (espresso/latte/cappuccino):").casefold()
+                match choise:
+                    case "espresso":
+                        return choise
+                    case "latte":
+                        return choise
+                    case "cappuccino":
+                        return choise
+                    case "off":
+                        exit()
+                    case "report":
+                        print("Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: {money}".format(water=self.resources["water"], milk=self.resources["milk"], coffee=self.resources["coffee"], money=self.bank))
 
-def prompt():
-    """Prompte user by asking"""
-    while True:
-        choise = input("What would you like? (espresso/latte/cappuccino):").casefold()
-        if choise == "espresso" or choise ==  "latte" or choise == "cappuccino" or choise == "offe":
-            return choise
+        def checkResources(resource_name, quantity):
+            """Check resources sufficient"""
+            if self.resources[resource_name] >= quantity:
+                return True
+            else:
+                return False
 
-# TODO: Turn off the Coffee Machine by entering “off” to the prompt.
-# TODO: Print report. when asked. List current resources values left.
-print("Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: {money}".format(water=resources["water"], milk=resources["milk"], coffee=resources["coffee"], money=bank))
-# TODO: Check resources sufficient?
+        def main():
+            drink = prompt()
+            for (key, value) in self.MENU[drink]["ingredients"].items():
+                if not checkResources(key, value):
+                    print("Sorry there is not enough {}".format(key))
+                    break
+            
+
+        if __name__ == '__main__':
+            main()
+
+
+CoffeeMachine()
+
 # TODO: Process coins.
 # TODO: Check transaction successful?
 # TODO: Make Coffee - If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the coffee machine resources.2
