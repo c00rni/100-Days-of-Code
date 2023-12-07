d3bb8611bd9a7b09a5f70ecf8cc5654b1dcc1b4a
corni <gmigan.a@gmail.com>
2023-12-07 18:15:58 +0100

    Finalized coffeeMachin as a OOP

--- coffee-machine/main.py
+++ coffee-machine/main.py
@@ -1,4 +1,29 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os 
+
+menu = Menu()
+maker = CoffeeMaker()
+bank = MoneyMachine()
+os.system('cls' if os.name == 'nt' else 'clear')
+while True:
+    user_input = input("What would you like? ({}):".format(menu.get_items()))
+    if user_input == "report":
+        maker.report()
+    if user_input == "off":
+        print("System Shutdown...")
+        exit()
+
+    item = menu.find_drink(user_input)
+    if not item:
+        continue
+    else:
+        if maker.is_resource_sufficient(item) and bank.make_payment(item.cost):
+            maker.make_coffee(item)
+            
+
+
+        
+    
 
