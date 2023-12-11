4917fc5801e3ec6973f296e98d5a6704bdccbb04
corni <gmigan.a@gmail.com>
2023-12-11 15:31:50 +0100

    Initialization of the Turtle Race project

---
+++ turtle-race/main.py
@@ -0,0 +1,8 @@
+from turtle import Turtle
+
+
+race = Turtle("turtle")
+race.screen.setup(width=600, height=400)
+#self._colors = set(["red", "orange", "yellow", "green", "blue", "purple"])
+
+race.screen.mainloop()
