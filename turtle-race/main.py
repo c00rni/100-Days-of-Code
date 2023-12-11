f49d14d5b08a20e35288e69caeead0e1b49fde57
corni <gmigan.a@gmail.com>
2023-12-11 16:21:08 +0100

    Working turtle race project

--- turtle-race/main.py
+++ turtle-race/main.py
@@ -1,8 +1,41 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+import random
 
 
-race = Turtle("turtle")
-race.screen.setup(width=600, height=400)
-#self._colors = set(["red", "orange", "yellow", "green", "blue", "purple"])
+screen = Screen()
+screen.setup(width=600, height=400)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+is_race_on = False
+while True:
+    user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race ? Enter a color: ")
+    if user_bet in colors:
+        is_race_on = True
+        break
 
-race.screen.mainloop()
+count = -100
+turtles = []
+
+for color in colors:
+    new_turtle = Turtle()
+    turtles.append(new_turtle)
+    new_turtle.shape(name="turtle")
+    new_turtle.color(color)
+    new_turtle.penup()
+    new_turtle.goto(x=-270, y=count)
+    count += 40
+
+winner = ""
+
+while is_race_on:
+    for turtle in turtles:
+        turtle.fd(random.randrange(11))
+        if turtle.xcor() > 300:
+            winner = turtle.pencolor()
+            is_race_on = False
+
+if user_bet == winner:
+    print(f"You won ! The winning turtle is {winner}")
+else:
+    print(f"You lost. The winning turtle is {winner}")
+
+screen.mainloop()
