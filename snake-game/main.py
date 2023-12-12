3d696f0fdf65ec824d9711fb314cad01e1d33b38
corni <gmigan.a@gmail.com>
2023-12-12 14:53:27 +0100

    Created a snake of 3 block and a background for the game

--- snake-game/main.py
+++ snake-game/main.py
@@ -5,3 +5,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+
+
+snack_body = []
+for i in range(3):
+    square = Turtle()
+    square.shapesize(stretch_wid=1, stretch_len=1)
+    square.shape("square")
+    square.color("white")
+    square.penup()
+    square.setpos(x=-20*i,y=0)
+    snack_body.append(square)
+
+screen.mainloop()
