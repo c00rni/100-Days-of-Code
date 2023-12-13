37103ed78658784f31bfc4d1cabbce029a9dce47
corni <gmigan.a@gmail.com>
2023-12-13 15:56:49 +0100

    Added a score which increase each time the user hit some food

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 
@@ -17,10 +19,17 @@ screen.onkey(snack.down, "Down")
 screen.onkey(snack.right, "Right")
 screen.onkey(snack.left, "Left")
 
+food = Food(screen=screen)
+score = ScoreBoard()
+
 game_is_on = True
 while game_is_on:
     snack.move()
     screen.update()
     time.sleep(0.1)
+
+    if snack.head.distance(food) < 15:
+        food.refresh()
+        score.increaseScore()
     
 screen.mainloop()
