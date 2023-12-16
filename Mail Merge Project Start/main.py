9d1934580244b28e93c7a274f65d92bea7faa5a4
corni <gmigan.a@gmail.com>
2023-12-16 11:51:36 +0100

    Script to generate letter with names

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -6,3 +6,13 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+base_letter = ""
+with open("Input/Letters/starting_letter.txt", "r") as base_letter_file:
+    base_letter = str(base_letter_file.read())
+
+with open("Input/Names/invited_names.txt", "r") as names_file:
+    for name in names_file:
+        name = name.strip()
+        with open(f'Output/ReadyToSend/{name}.txt', mode='w', encoding='utf-8') as new_letter_file:
+            new_letter_file.write(base_letter.replace("[name]", name))
