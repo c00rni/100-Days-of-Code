dbdc099f0f88857c6417c8a963a057df3105858a
corni <gmigan.a@gmail.com>
2023-12-30 11:10:03 +0100

    Initaialization of Birtday whisher project

---
+++ Birthday-Wishes/Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,6 @@
+# TODO: Send a mail with python
+# send a mail a specific date with datetime module
+# send a motivation quote on mondays
+# list birthdays in a csv file
+# Automate a birthday wisher 
+# Run the script on the cloud pythonanywhere.com
