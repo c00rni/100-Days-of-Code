dc92c331ed2d0313c0dded4b321f9f4c97e81133
corni <gmigan.a@gmail.com>
2024-01-02 15:55:56 +0100

    Send mail at a specified date

--- Birthday-Wishes/Birthday Wisher (Day 32) start/main.py
+++
@@ -1,6 +0,0 @@
-# TODO: Send a mail with python
-# send a mail a specific date with datetime module
-# send a motivation quote on mondays
-# list birthdays in a csv file
-# Automate a birthday wisher 
-# Run the script on the cloud pythonanywhere.com
---
+++ Birthday-Wishes/main.py
@@ -0,0 +1,46 @@
+# TODO: Send a mail with python
+# send a mail a specific date with datetime module
+# send a motivation quote on mondays
+# list birthdays in a csv file
+# Automate a birthday wisher 
+# Run the script on the cloud pythonanywhere.com
+
+
+# smtp.gmail.com
+
+import smtplib
+import datetime as dt
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv("../.env")
+
+GMAIL_ADDRESS = os.environ.get("GMAIL_ADDRESS")
+GMAIL_PASSWORD = os.environ.get("GMAIL_PASSWORD")
+
+
+subject = "[TEST] Antony python"
+body = "Hello sir !"
+sender = GMAIL_ADDRESS
+recipients = ["gmigan.a@gmail.com"]
+password = GMAIL_PASSWORD
+
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+
+def dayChecker(year=None, month=None, day=None):
+    today = dt.datetime.today()
+    if today.year == year and today.month == month and today.day == day:
+        send_email(subject, body, sender, recipients, password)
+    
+dayChecker(2024, 1, 2)
