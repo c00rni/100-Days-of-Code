62e9eb8f471a39bee4281868b467fca81c3f7992
corni <gmigan.a@gmail.com>
2024-01-03 19:07:10 +0100

    Used OOP

--- iss-over-head/main.py
+++ iss-over-head/main.py
@@ -3,56 +3,72 @@ from dotenv import load_dotenv
 from datetime import datetime
 import os
 import pytz
+from time import sleep
 
 load_dotenv("../.env")
 
 IPSTACK_API_KEY = os.environ.get("IPSTACK_API_KEY")
 CLAUSE_DISTANCE = 5
 
-#if os.path.isfile("user-position.txt"):
-try:
-    with open("user-position.txt") as user_position_file:
-        user_lat = float(user_position_file.readline().rstrip())
-        user_long = float(user_position_file.readline().rstrip())
-except OSError:
-    ip = get('https://api.ipify.org').content.decode('utf8')
-    r = get(f"http://api.ipstack.com/{ip}?access_key={IPSTACK_API_KEY}")
-    user_lat = float(r.json()['latitude'])
-    user_long = float(r.json()['longitude'])
-    with open("user-position.txt", "w") as user_position_file:
-        user_position_file.write(f"{user_lat}\n{user_long}")
+class ISSTracker:
 
+    def __init__(self) -> None:
+        self._setNightTime()
 
-payload = {'lat': user_lat, 'lng': user_long, 'formatted':"0"}
-r = get("https://api.sunrise-sunset.org/json", params=payload)
-sunset_date_str = r.json()['results']["sunset"]
-sunrise_date_str = r.json()['results']["sunrise"]
+    def _setUserPosition(self):
+        try:
+            with open("user-position.txt") as user_position_file:
+                self._user_lat = float(user_position_file.readline().rstrip())
+                self._user_long = float(user_position_file.readline().rstrip())
+        except OSError:
+            ip = get('https://api.ipify.org').content.decode('utf8')
+            r = get(f"http://api.ipstack.com/{ip}?access_key={IPSTACK_API_KEY}")
+            self._user_lat = float(r.json()['latitude'])
+            self._user_long = float(r.json()['longitude'])
+            with open("user-position.txt", "w") as user_position_file:
+                user_position_file.write(f"{self._user_lat}\n{self._user_long}")
 
-r = get("http://api.open-notify.org/iss-now.json")
-iss_postisions = r.json()["iss_position"]
-iss_lat = float(r.json()["iss_position"]["latitude"])
-iss_long = float(r.json()["iss_position"]["longitude"])
+    def _setNightTime(self):
+        self._setUserPosition()
+        payload = {'lat': self._user_lat, 'lng': self._user_long, 'formatted':"0"}
+        r = get("https://api.sunrise-sunset.org/json", params=payload)
+        sunset_date_str = r.json()['results']["sunset"]
+        sunrise_date_str = r.json()['results']["sunrise"]
 
-sunset_time = datetime.fromisoformat(sunset_date_str).replace(tzinfo=pytz.UTC)
-sunrise_time = datetime.fromisoformat(sunrise_date_str).replace(tzinfo=pytz.UTC)
-currnet_date = datetime.now().replace(tzinfo=pytz.UTC)
+        self._sunset_time = datetime.fromisoformat(sunset_date_str).replace(tzinfo=pytz.UTC)
+        self._sunrise_time = datetime.fromisoformat(sunrise_date_str).replace(tzinfo=pytz.UTC)
+        self._currnet_date = datetime.now().replace(tzinfo=pytz.UTC)
 
-is_night = (sunset_time < currnet_date or sunrise_time > currnet_date)
+    def _setISSPosition(self):
+        r = get("http://api.open-notify.org/iss-now.json")
+        self._iss_lat = float(r.json()["iss_position"]["latitude"])
+        self._iss_long = float(r.json()["iss_position"]["longitude"])
 
-if user_lat > 0:
-    lat_is_close = abs(iss_lat - user_lat) <= CLAUSE_DISTANCE
-else:
-    lat_is_close = abs(iss_lat + user_lat) <= CLAUSE_DISTANCE
-if user_long > 0:
-    long_is_close = abs(iss_long - user_long) <= CLAUSE_DISTANCE
-else:
-    long_is_close = abs(iss_long + user_long) <= CLAUSE_DISTANCE
+    def _isNight(self):
+        return (self._sunset_time < self._currnet_date or self._sunrise_time > self._currnet_date)
 
-is_close = lat_is_close and long_is_close
+    def _issIsClause(self):
+        self._setISSPosition()
+        if self._user_lat > 0:
+            lat_is_close = abs(self._iss_lat - self._user_lat) <= CLAUSE_DISTANCE
+        else:
+            lat_is_close = abs(self._iss_lat + self._user_lat) <= CLAUSE_DISTANCE
+        if self._user_long > 0:
+            long_is_close = abs(self._iss_long - self._user_long) <= CLAUSE_DISTANCE
+        else:
+            long_is_close = abs(self._iss_long + self._user_long) <= CLAUSE_DISTANCE
+        return lat_is_close and long_is_close
+    
+    def isISSVisible(self):
+        if self._isNight() and self._issIsClause():
+            self._notify()
 
-print(f"Night: {is_night}")
-print(f"User: {user_lat}, {user_long}")
-print(f"ISS: {iss_lat}, {iss_long}")
+    def _notify(self):
+        print("Iss visible !!")
 
-if is_close and is_night:
-    pass
+    
+
+tracker = ISSTracker()
+while True:
+    tracker.isISSVisible()
+    sleep(1000*60*5)
