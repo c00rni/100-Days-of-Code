5f4fa32408d2db957980486a84c562525ba22f01
corni <gmigan.a@gmail.com>
2024-01-04 14:11:33 +0100

    Data are fetched from the trivia database

--- quizzler-app-start/main.py
+++ quizzler-app-start/main.py
@@ -1,9 +1,11 @@
 from question_model import Question
-from data import question_data
+from data import QuestionGenerator
 from quiz_brain import QuizBrain
 
 question_bank = []
-for question in question_data:
+questions = QuestionGenerator().getQuestions()
+print(questions)
+for question in questions:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
