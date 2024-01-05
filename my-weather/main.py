26a4ea0e1245b25496a777a39d8f87a9875fced5
corni <gmigan.a@gmail.com>
2024-01-05 16:03:09 +0100

    Print on the console a description of the weather and the cloudiness pourcentage

--- my-weather/main.py
+++ my-weather/main.py
@@ -2,15 +2,21 @@
 # TODO: Request forecast API
 # TODO: print status code
 # TODO: print the response to the console
-# TODO: print the weather id and secription
+# TODO: print the weather description
 
 import os
 from dotenv import load_dotenv
+from requests import get
 
 load_dotenv("../.env")
 
-HOME_LONGITUDE = os.environ.get("HOME_LONGITUDE")
-HOME_LATITUDE = os.environ.get("HOME_LATITUDE")
+OPEN_WEATHER_API_KEY = os.environ.get("OPEN_WEATHER_API")
 
-print(HOME_LATITUDE)
-print(HOME_LONGITUDE)
+payload = {'q': "nyon", 'appid': OPEN_WEATHER_API_KEY}
+r = get("https://api.openweathermap.org/data/2.5/weather", params=payload)
+if r.status_code == 200:
+    description = r.json()["weather"][0]["description"]
+    cloudiness_pourcentage = r.json()["clouds"]["all"]
+    print(f"Decription: {description}, Cloud: {cloudiness_pourcentage}%.")
+else:
+    print("Request to openweathermap API failed.")
