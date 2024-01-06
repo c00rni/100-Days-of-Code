400c1b48b561bce72b91ffb91e577555cd683509
corni <gmigan.a@gmail.com>
2024-01-07 00:04:24 +0100

    Print the 3 first headline when a price deferrence of more than 5% between yesterday and the day before occur

--- stock-news-extrahard-start/main.py
+++ stock-news-extrahard-start/main.py
@@ -7,9 +7,10 @@ from pytz import timezone
 load_dotenv("../.env")
 
 ALPHA_VENTURE_API_KEY = os.environ.get("ALPHA_VENTURE_API_KEY")
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
 
 STOCK_SYMBOL = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+COMPANY_NAME = "Tesla"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -37,15 +38,23 @@ Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and 
 # https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=TSLA&apikey=demo
 
 payload = {f"function": "TIME_SERIES_DAILY", "symbol": {STOCK_SYMBOL}, "outputsize": "compact", "apikey": {ALPHA_VENTURE_API_KEY}}
-r = get("https://www.alphavantage.co/query", params=payload)
+alpha_vantage_request = get("https://www.alphavantage.co/query", params=payload)
 
 date_1 = (datetime.today() - timedelta(days=1))
 date_2 = (date_1 - timedelta(days=1)).strftime("%Y-%m-%d")
 date_1 = date_1.strftime("%Y-%m-%d")
+try:
+    date1_market_data = alpha_vantage_request.json()['Time Series (Daily)'][date_1]
+    date2_market_data = alpha_vantage_request.json()['Time Series (Daily)'][date_2]
 
-date1_market_data = r.json()['Time Series (Daily)'][date_1]
-date2_market_data = r.json()['Time Series (Daily)'][date_2]
+    change_pourcentage = (abs(float(date2_market_data['4. close']) - float(date1_market_data['4. close'])) / float(date2_market_data['4. close'])) * 100
 
-change_pourcentage = (abs(float(date2_market_data['4. close']) - float(date1_market_data['4. close'])) / float(date2_market_data['4. close'])) * 100 
-if change_pourcentage > 5:
-    print("Get News")
+
+    if change_pourcentage > 5:
+        payload = {"q": COMPANY_NAME, "pageSize": 3, "page": 0, "apiKey": NEWS_API_KEY}
+        r = get("https://newsapi.org/v2/top-headlines", params=payload)
+        articles = r.json()['articles']
+        for article in articles:
+            print(article['title'])
+except Exception:
+    print(alpha_vantage_request.json().get('Information'))
