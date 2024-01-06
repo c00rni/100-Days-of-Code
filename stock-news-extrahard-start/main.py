5f3c4eeb2f34738bc80406328cd28fdd3609ba26
corni <gmigan.a@gmail.com>
2024-01-06 23:28:05 +0100

    Print 'Get news' to the console when the price moved more than 5 pourcent between yesterday and the day before

--- stock-news-extrahard-start/main.py
+++ stock-news-extrahard-start/main.py
@@ -1,4 +1,14 @@
-STOCK = "TSLA"
+import os
+from dotenv import load_dotenv
+from requests import get
+from datetime import datetime, timedelta
+from pytz import timezone
+
+load_dotenv("../.env")
+
+ALPHA_VENTURE_API_KEY = os.environ.get("ALPHA_VENTURE_API_KEY")
+
+STOCK_SYMBOL = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
@@ -22,3 +32,20 @@ Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
 
+
+# Search for symbol
+# https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=TSLA&apikey=demo
+
+payload = {f"function": "TIME_SERIES_DAILY", "symbol": {STOCK_SYMBOL}, "outputsize": "compact", "apikey": {ALPHA_VENTURE_API_KEY}}
+r = get("https://www.alphavantage.co/query", params=payload)
+
+date_1 = (datetime.today() - timedelta(days=1))
+date_2 = (date_1 - timedelta(days=1)).strftime("%Y-%m-%d")
+date_1 = date_1.strftime("%Y-%m-%d")
+
+date1_market_data = r.json()['Time Series (Daily)'][date_1]
+date2_market_data = r.json()['Time Series (Daily)'][date_2]
+
+change_pourcentage = (abs(float(date2_market_data['4. close']) - float(date1_market_data['4. close'])) / float(date2_market_data['4. close'])) * 100 
+if change_pourcentage > 5:
+    print("Get News")
