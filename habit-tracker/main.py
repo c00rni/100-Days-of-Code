2648f0d0eaba8c4a7e70e7fb50eb553ee362a316
corni <gmigan.a@gmail.com>
2024-01-07 20:59:13 +0100

    Added a function to create users on pixela

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,4 +1,7 @@
-from pixela import Pixela
+from requests import get, post, put
+from random import choice
+from re import fullmatch
+
 # function to Create user
 
 # print the link to view my account
@@ -6,3 +9,42 @@ from pixela import Pixela
 
 # function to create a new grapth
 # put /v1/users/<username>/graphs/<graphID>/<yyyyMMdd>
+
+
+class HabitTracker:
+
+    def __init__(self) -> None:
+        pass
+
+    def createUser(self, username, agree_terms="yes", not_minor="yes") -> None:
+        if not fullmatch(pattern="[a-z][a-z0-9-]{1,32}", string=username):
+            raise ValueError("The username must be in lower case and digist only")
+        
+        self._token = self._createToken()
+        payload = {
+            "token": self._token,
+            "username":username,
+            "agreeTermsOfService":agree_terms,
+            "notMinor":not_minor
+        }
+        request = post("https://pixe.la/v1/users", json=payload)
+        if request.status_code != 200:
+            raise ValueError(request.json()['message'])
+        print(f"Created the new username: {username}\nYour unique token is: {self._token}")
+        print(request.json()['message'])
+
+    
+    def _createToken(self) -> str:
+        letter = list("abcdefghijklmnopqrstuvwsyz0123456789")
+        digits = list()
+        token = ""
+
+        for _ in range(128):
+            token += choice(digits+letter) 
+        return token
+
+
+
+tracker = HabitTracker()
+
+tracker.createUser("corni")
