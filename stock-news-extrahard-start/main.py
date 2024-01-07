ce1f399adce782a36c8d226be89f2d16d39b2f04
corni <gmigan.a@gmail.com>
2024-01-07 01:01:52 +0100

    Send a formated SMS when a significant change in price is detected

--- stock-news-extrahard-start/main.py
+++ stock-news-extrahard-start/main.py
@@ -3,39 +3,20 @@ from dotenv import load_dotenv
 from requests import get
 from datetime import datetime, timedelta
 from pytz import timezone
+from notifier import SMSNotifier
 
 load_dotenv("../.env")
 
 ALPHA_VENTURE_API_KEY = os.environ.get("ALPHA_VENTURE_API_KEY")
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER")
+PERSONAL_NUMBER = os.environ.get("PERSONAL_NUMBER")
 
 STOCK_SYMBOL = "TSLA"
 COMPANY_NAME = "Tesla"
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-## STEP 3: Use https://www.twilio.com / Pywhatkit library
-# Send a seperate message with the percentage change and each article's title and description to through whatapp. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
-
-# Search for symbol
-# https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=TSLA&apikey=demo
 
 payload = {f"function": "TIME_SERIES_DAILY", "symbol": {STOCK_SYMBOL}, "outputsize": "compact", "apikey": {ALPHA_VENTURE_API_KEY}}
 alpha_vantage_request = get("https://www.alphavantage.co/query", params=payload)
@@ -47,14 +28,20 @@ try:
     date1_market_data = alpha_vantage_request.json()['Time Series (Daily)'][date_1]
     date2_market_data = alpha_vantage_request.json()['Time Series (Daily)'][date_2]
 
-    change_pourcentage = (abs(float(date2_market_data['4. close']) - float(date1_market_data['4. close'])) / float(date2_market_data['4. close'])) * 100
+    value_difference = float(date2_market_data['4. close']) - float(date1_market_data['4. close'])
+    change_pourcentage = (value_difference / float(date2_market_data['4. close'])) * 100
 
 
     if change_pourcentage > 5:
         payload = {"q": COMPANY_NAME, "pageSize": 3, "page": 0, "apiKey": NEWS_API_KEY}
         r = get("https://newsapi.org/v2/top-headlines", params=payload)
         articles = r.json()['articles']
+        sign = "🔺" if value_difference > 0 else "🔻"
+        message = f"\n\n{sign} change_pourcentage %\n"
+        sms_notifier = SMSNotifier(sender_number=TWILIO_NUMBER, receiver_number=PERSONAL_NUMBER, username=TWILIO_ACCOUNT_SID, password=TWILIO_AUTH_TOKEN)
+        
         for article in articles:
-            print(article['title'])
+            message += f"Headline: {article['title']}\nBrief: {article['description']}\n"
+        sms_notifier.sendMessage(message)
 except Exception:
     print(alpha_vantage_request.json().get('Information'))
