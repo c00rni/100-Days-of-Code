e882edf21cfb8a4885750f3b0dad96034a902b2a
corni <gmigan.a@gmail.com>
2024-01-08 18:35:40 +0100

    Add workout duration and the calories spent to a google sheet

--- workout-tracking/main.py
+++ workout-tracking/main.py
@@ -2,13 +2,17 @@ from requests import post
 import os
 import pandas as pd
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv("../.env")
 
 NUTRITION_API_ID = os.environ.get("NUTRITION_API_ID")
 NUTRITION_API_KEY = os.environ.get("NUTRITION_API_KEY")
+WORKOUT_PROJECT_CREDS = os.environ.get("WORKOUT_PROJECT_CREDS")
 
-# TODO: 1. Using the Nutritionix "Natural Language for Exercise" API Documentation, figure out how to print the exercise stats for plain text input.
+SHEETY_USERNAME = os.environ.get("SHEETY_USERNAME")
+SHEETY_PROJECT_NAME = os.environ.get("SHEETY_PROJECT_NAME")
+SHEET_ENDPOINT = os.environ.get("SHEET_ENDPOINT")
 
 domain = "https://trackapi.nutritionix.com"
 exercice_endpoint = "/v2/natural/exercise"
@@ -21,4 +25,27 @@ data = {
 }
 
 r = post(f"{domain}{exercice_endpoint}", headers=headers, json=data)
-print(r.json())
+exercice_datas = r.json()['exercises']
+
+date = datetime.now()
+for exercice in exercice_datas:
+
+    new_row_data = {
+        SHEET_ENDPOINT: {       
+        "date": date.strftime("%d/%m/%Y"),
+        "time": date.strftime("%H:%M:%S"),
+        "exercise": exercice['user_input'],
+        "duration": float(exercice['duration_min']),
+        "calories": float(exercice['nf_calories'])
+        }
+    }
+
+    headers = {
+        "Authorization": f"Basic {WORKOUT_PROJECT_CREDS}"
+    }
+    r = post(f"https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT_NAME}/{SHEET_ENDPOINT}", headers=headers, json=new_row_data)
+    error = r.json().get("error")
+    if error:
+        print(error)
+    else:
+        print(r.json())
