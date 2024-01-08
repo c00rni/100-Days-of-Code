b8a54e25ac76ffcdc05ddd6a4b9066c988f9ca9a
corni <gmigan.a@gmail.com>
2024-01-08 16:38:14 +0100

    Make a colorie estimation of a exercice

--- workout-tracking/main.py
+++ workout-tracking/main.py
@@ -1,3 +1,4 @@
+from requests import post
 import os
 import pandas as pd
 from dotenv import load_dotenv
@@ -9,3 +10,15 @@ NUTRITION_API_KEY = os.environ.get("NUTRITION_API_KEY")
 
 # TODO: 1. Using the Nutritionix "Natural Language for Exercise" API Documentation, figure out how to print the exercise stats for plain text input.
 
+domain = "https://trackapi.nutritionix.com"
+exercice_endpoint = "/v2/natural/exercise"
+headers = {
+    'x-app-id': NUTRITION_API_ID,
+    'x-app-key': NUTRITION_API_KEY
+}
+data = {
+    "query": input("Tell me wish execice you did? ")
+}
+
+r = post(f"{domain}{exercice_endpoint}", headers=headers, json=data)
+print(r.json())
